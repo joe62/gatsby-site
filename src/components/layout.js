449aab0b5f756e3d07f62463f626f6bb74309df8
joe62 <yunfei_z@163.com>
2023-07-23 16:13:52 +0800

    Finished Gatsby Tutorial Part 2

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import * as styles from './layout.module.css'
 
 const Layout = ({ pageTitle, children }) => {
   return (
-    <div>
+    <div className={styles.container}>
       <nav>
-        <ul>
-          <li><Link to='/'>Home</Link></li>
-          <li><Link to='/about'>About</Link></li>
+        <ul className={styles.navLinks}>
+          <li className={styles.navLinkItem}><Link className={styles.navLinkText} to='/'>Home</Link></li>
+          <li className={styles.navLinkItem}><Link className={styles.navLinkText} to='/about'>About</Link></li>
         </ul>
       </nav>
       <main>
-        <h1>{pageTitle}</h1>
+        <h1 className={styles.heading}>{pageTitle}</h1>
         {children}
       </main>
     </div>
